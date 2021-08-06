15047332747fb3d328eb7445580bbb0dc693585c
alextk1978 <alextk2017@gmail.com>
2021-08-06 14:52:40 +0700

    Создание и удаление таблицы

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,14 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Statement statement = Util.getStatement();
-
+        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -9,12 +13,39 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-    public void createUsersTable() {
+    public static final String TABLE_NAME = "`users`.`new_table`";
+    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+            "`id` INT NOT NULL AUTO_INCREMENT, " +
+            "`name` VARCHAR(45) NOT NULL, " +
+            "`lastName` VARCHAR(45) NULL, " +
+            "`age` TINYINT(3) NULL, " +
+            "PRIMARY KEY (`id`)) " +
+            "ENGINE = InnoDB " +
+            "DEFAULT CHARACTER SET = utf8";
+    public static final String DROP_TABLE = "DROP TABLE " + TABLE_NAME;
 
+    public void createUsersTable() {
+        try (Connection connection = Util.getConnection();
+            Statement statement = connection.createStatement()){
+            boolean exists = connection.getMetaData().getTables(null, null, TABLE_NAME, null).next();
+            if (!exists) {
+                statement.executeUpdate(CREATE_TABLE);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()){
+            boolean exists = connection.getMetaData().getTables(null, null, TABLE_NAME, null).next();
+            if (exists) {
+                statement.executeUpdate(DROP_TABLE);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
